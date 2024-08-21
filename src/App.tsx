eb61602fdf31d8a5a65aaf149123344da8b39b1f
Omerkugel12 <omerkugel12@gmail.com>
2024-08-21 11:51:19 +0300

    react set-up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <p>hii</p>
+      <Button className="bg-blue_1">hiii</Button>
+    </>
+  );
+}
+
+export default App;
