a9c192f6c870588bf8bc55b761232bd4d355e474
Omerkugel12 <omerkugel12@gmail.com>
2024-08-21 18:11:29 +0300

    18:11

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { Button } from "./ui/button";
 
 function Header() {
   return (
@@ -6,9 +6,19 @@ function Header() {
       <div className="flex justify-between items-center">
         <div className="size-56">
           <img src="/src/Booking_Com_Logotype_Aug2020_White_Blue-BG.png" />
-          {/* <h1 className="text-3xl">Booking.com</h1> */}
         </div>
-        <div></div>
+        <div className="flex flex-row-reverse">
+          <Button>Sign in</Button>
+          <Button>Register</Button>
+          <Button>List your property</Button>
+          <Button>?</Button>
+          <Button className="bg-blue_1">
+            <div className=" flex justify-center size-7 rounded-full overflow-hidden ">
+              <img src="/src/US.png" alt="" />
+            </div>
+          </Button>
+          <Button></Button>
+        </div>
       </div>
     </nav>
   );
